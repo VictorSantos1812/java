f71eb26186f440f550fcd4cc874dff96c6dfb90c
VictorSantos1812 <victorsantos1812@icloud.com>
2021-03-24 17:40:16 -0300

    teste

--- sintaxe-variaveis-fluxo/src/TestePontoFlutuante.java
+++ sintaxe-variaveis-fluxo/src/TestePontoFlutuante.java
@@ -6,7 +6,7 @@ public class TestePontoFlutuante {
 		double salario = 1250.70;
 		System.out.println("meu salario é: " + salario);
 		
-		double idade = 37;
+		//double idade = 37;
 		
 		double divisao = 3.14 / 2;
 		System.out.println(divisao);
