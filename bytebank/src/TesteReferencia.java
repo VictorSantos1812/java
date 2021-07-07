553d7ad1af05da764900e9a68f48dd33c8e9c1a4
VictorSantos1812 <victorarruda_santos@hotmail.com>
2021-07-07 12:22:07 -0300

    teste4

--- bytebank/src/TesteReferencia.java
+++ bytebank/src/TesteReferencia.java
@@ -1,25 +1,25 @@
-public class TesteReferencia {
-	public static void main(String[] args) {
-		Conta primeiraConta = new Conta();
-		primeiraConta.saldo = 300;
+// public class TesteReferencia {
+// 	public static void main(String[] args) {
+// 		Conta primeiraConta = new Conta();
+// 		primeiraConta.saldo = 300;
 		
-		System.out.println("saldo da primeria: " + primeiraConta.saldo);
+// 		System.out.println("saldo da primeria: " + primeiraConta.saldo);
 		
-		Conta segundaConta = primeiraConta;
+// 		Conta segundaConta = primeiraConta;
 		
-		System.out.println("saldo da segunda: " + segundaConta.saldo);
-		segundaConta.saldo += 100;
+// 		System.out.println("saldo da segunda: " + segundaConta.saldo);
+// 		segundaConta.saldo += 100;
 		
-		System.out.println(primeiraConta.saldo);
-		System.out.println(segundaConta.saldo);
+// 		System.out.println(primeiraConta.saldo);
+// 		System.out.println(segundaConta.saldo);
 		
-		if(primeiraConta == segundaConta) {
-			System.out.println("mesma conta");
-		}else {
-			System.out.println("contas diferentes");
-		}
+// 		if(primeiraConta == segundaConta) {
+// 			System.out.println("mesma conta");
+// 		}else {
+// 			System.out.println("contas diferentes");
+// 		}
 		
-		System.out.println(primeiraConta);
-		System.out.println(segundaConta);
-	}
-}
+// 		System.out.println(primeiraConta);
+// 		System.out.println(segundaConta);
+// 	}
+// }
